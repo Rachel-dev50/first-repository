e8ae0d232c93343d37f4405194a41be379be8e03
IKUZWE Rachel <ikuzwerachel@gmail.com>
2025-03-20 11:41:38 -0700

    conversation from one type to other typer

---
+++ catena.js
@@ -0,0 +1,13 @@
+let a="1234";
+// conversation to string
+let b=string(a);
+console.log(b);
+// conversation to numbers
+let c=number(b);
+console.log(c);
+// comversation to bigint
+let d=bigInt(c);
+console.log(d);
+// conversation to boolean
+let e=boolean(d);
+console.log(e);
