06293f751e13a0fe314cab5531fe48f3cd687e1b
IKUZWE Rachel <ikuzwerachel@gmail.com>
2025-03-23 06:25:09 -0700

    to check the  section of student according to marks

---
+++ marks.js
@@ -0,0 +1,9 @@
+let marks;
+if (marks>=80)and(marks<=100);
+console.log(first - honour);
+elseif (marks>=70)&&(marks<=79);
+console.log(upper- honour);
+elseif (marks>=60)&&(marks<=69);
+console.log(lower - honour);
+elseif (marks>=50)&&(marks<=59);
+console.log(pass);
