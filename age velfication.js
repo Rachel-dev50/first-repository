b8cbbb28378facfdde5def1f29cafb40a2fe836a
IKUZWE Rachel <ikuzwerachel@gmail.com>
2025-03-29 11:42:51 -0700

    to check if person havo to vote according to years

---
+++ age velfication.js
@@ -0,0 +1,11 @@
+//to ask how old are you
+let age=prompt("how old are you?");
+//to convert age into number
+age=Number(age);
+//to check the age
+if(age>=18){
+    alert("you are able to vote");
+}
+else{
+    alert("you are not able to vote");
+}
